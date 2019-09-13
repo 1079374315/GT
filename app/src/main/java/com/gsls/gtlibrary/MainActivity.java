dbd3de7b4d9793c2370246558c4454fe1ba9c199
GSLS <1079374315@qq.com>
2019-09-13 20:50:23 +0800

    * 更新时间:2019.9.13  * <p>  * <p>  * 更新内容：（1.0.7 版本 大更新）  * 1.新增 AndroidUtilCode 工具包 （如果不想初始化加载可在初始化GT前调用：GT.setIsGTUtil(false);）  * 2.新增 Animator（真/假）动画工具包  * 3.新增 HttpUtil 原始网络请求 GET / POST  * 4.优化GT_Object、GT_List、GT_Set、GT_Map 的用法。

--- app/src/main/java/com/gsls/gtlibrary/MainActivity.java
+++ app/src/main/java/com/gsls/gtlibrary/MainActivity.java
@@ -7,6 +7,8 @@ import com.gsls.gtlibrary.util.GT;
 @GT.Annotations.GT_Activity(R.layout.activity_main)
 public class MainActivity extends GT.AnnotationActivity {
 
+
+
     @Override
     protected void initView(Bundle savedInstanceState) {
         build(this);//绑定 Activity
